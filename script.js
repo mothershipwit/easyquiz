c2020ac1c2a92d815b1f4567bbc3713dfd8510da
Witni <witni.whittle@icloud.com>
2020-07-21 11:34:38 -0500

    updated script and index

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
-<script>
-    {
-        function getAnswers(field) {
+
+        function getAnswers(field)
+         {
             console.log(field);
             let answer = document.getElementById(field);
             return answer.value;
-}
+        }
 
-}      
-    function checkAnswers() {
+      
+
+        function checkAnswers() {
         let answers = ["Earth", "Pink", "Calves", "8,Eight", "Duck"];
         
         for (let i = 0; i < 5; i++) {
-                let }fieldId = i + 1;
+                let } fieldId = i + 1;
                 let answer = checkAnswers("a" + fieldId);
 
                 gradeAnswer(fieldId, answer == answers[i]);
          }
         
-{"}"
-        function checkAnswers(field, isRight);
-        console.log(checkAnswers);
+{
+         checkAnswers(field, isRight)
+        { console.log(checkAnswers);
             let gradeClass = isRight ? "answer right" : "answer wrong";
-            let gradeIcon = isRight ? '<i class="fas fa-check-circle"></i> : <i class="fas fa-skull-crossbones"></i></i>';
-            document.getElementById("a" + field).className = gradeClass;
+            let gradeIcon = isRight ? '<i class="fas fa-check-circle"></i> : <i class="fas fa-skull-crossbones"></i></i>':
+            document.getElementById(`a${field}`).className = gradeClass;
             document.getElementById("icon" + field).innerHTML = gradeIcon;
         
         
         function clearAnswers() 
-            for (let i = 1; i < 6; i++)
+             (let); i = 1; i < 6; i++;
                 let field = document.getElementById("a" + i);
                 field.value = null;
                 field.className = null;
                 document.getElementById("icon" + i).innerHTML = "null";
                }
             }
-</script>
